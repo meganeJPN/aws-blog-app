06de4bb4dbedbce834fffd4d82bc64819816b75e
meganeJPN <ooosamuuu1985@gmail.com>
2020-12-21 00:28:42 +0900

    socketが見つからないエラーを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
 # デプロイするアプリケーション名
-set :application, 'blog-app'
+set :application, 'achieve'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/meganeJPN/aws-blog-app'
